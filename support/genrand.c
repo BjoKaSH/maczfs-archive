94096bdefb3391e7f7e9e703ea96a61efd5ebe40
BjoKaSH <googlelogin@bjoern-kahl.de>
2012-11-11 21:10:17 +0100

    Tried to optimize output function and to implement trinagular distribution.
    Something broke, this version produces a really bad distribution of numbers.

--- support/genrand.c
+++ support/genrand.c
@@ -31,7 +31,7 @@ extern int optopt;
 extern int opterr;
 extern int optreset;
 
-char *option_string = "s:S:m:M:c:bwldhao";
+char *option_string = "s:S:m:M:c:bwldhaotHv";
 
 static struct option long_options[] = {
 	{"seed", required_argument, 0, 's'},
@@ -46,27 +46,30 @@ static struct option long_options[] = {
 	{"hex", no_argument, 0, 'h'},
 	{"ascii", no_argument, 0, 'a'},
 	{"octets", no_argument, 0, 'o'},
-	{"help", no_argument, 0, '?'},
+  {"triangular", no_argument, 0, 't'},
+  {"verbose", no_argument, 0, 't'},
+	{"help", no_argument, 0, 'H'},
 	{0, 0, 0, 0}
 };
 
 void print_help(void) {
   fprintf(stdout, "%s\n",
   " Usage:\n"
-  " genrand -s seed -S statefile -n num_units -b -w -l -d -h -a -o -t \n"
-  " -s seed : seed value, number in the range 0 < 2^32-1\n"
-  " -S statefile : file to store internal state between invocations\n"
-  " -m minvalue : minimal output vale to produce\n"
-  " -M maxvalue : maximum output value to produce\n"
-  " -c num_units : number of output data units\n"
-  " -b : output bytes\n"
-  " -w : output words (16-bits)\n"
-  " -l : output longs (32-bits)\n"
-  " -d : output units as decimal strings, one unit per line\n"
-  " -h : output units in hex-coding, one unit per line\n"
-  " -a : output units as string in base64-coding\n"
-  " -o : output units in binary coding, i.e. as one, two or four octets per unit\n"
-  " -t : produce triangular distributed numbers with limits -m and -M\n"
+  " genrand -s seed -S statefile -n num_units -b -w -l -d -h -a -o -t -v\n"
+  " -s seed : seed value, number in the range 0 < 2^32-1, --seed\n"
+  " -S statefile : file to store internal state between invocations, --statefile\n"
+  " -m minvalue : minimal output vale to produce, --min\n"
+  " -M maxvalue : maximum output value to produce, --max\n"
+  " -c num_units : number of output data units, --count\n"
+  " -b : output bytes, --bytes\n"
+  " -w : output words (16-bits), --words\n"
+  " -l : output longs (32-bits), --longs\n"
+  " -d : output units as decimal strings, one unit per line, --decimal\n"
+  " -h : output units in hex-coding, one unit per line, --hex\n"
+  " -a : output units as string in base64-coding, --ascii\n"
+  " -o : output units in binary coding, i.e. as one, two or four octets per unit, --octets\n"
+  " -t : produce triangular distributed numbers with limits -m and -M, --triangular\n"
+  " -v : be verbose, may be repeated to increase level, --verbose\n"
   " \n"
   " -m -M -a and -t are not implemented.\n");
 }
@@ -75,6 +78,10 @@ int opt_mode_fmt = 'd';
 int opt_mode_fmt_cnt = 0;
 int opt_mode_size = 'b';
 int opt_mode_size_cnt = 0;
+int opt_mode_size_bits = 8;
+unsigned long opt_mode_size_mask = 255; /* 2^opt_mode_size_bits - 1 */
+unsigned long opt_scale = 0; /* (opt_max - opt_min +1) */
+unsigned long opt_minmax = 0; /* (opt_min + opt_max)/2 */
 char *opt_seed = 0;
 unsigned long opt_min = 0;
 unsigned long opt_max = 0;
@@ -82,7 +89,8 @@ unsigned long long opt_count = 0;
 char *opt_statefile = 0;
 unsigned long long opt_seed_lcg = 0;
 unsigned long long opt_seed_lfsr = 0;
-
+int opt_tri = 0;
+int opt_verb = 0;
 
 /*
  * The first prng implements a linear congruential generator, as used
@@ -130,7 +138,7 @@ void lcg_iterate(void) {
       lcg_bits2 = lcg_val;
     lcg_bits2_left += lcg_param_m;
   }
-  lcg_bits = (unsigned long)lcg_bits2;
+  lcg_bits = (unsigned long)(lcg_bits2 & 0x0ffffffff);
   lcg_bits2 = lcg_bits2 >> 32;
   lcg_bits2_left -= 32;
 #endif
@@ -192,6 +200,7 @@ int lcg_read_state(FILE *fp, char *readbuffer) {
       } else
       if (strcmp(key, "lcg_param_m") == 0) {
         lcg_param_m = val;
+        lcg_param_m2 = ((unsigned long long)1 << lcg_param_m) -1;
         stateflags |= 4;
       } else
       if (strcmp(key, "lcg_val") == 0) {
@@ -226,7 +235,7 @@ int lcg_read_state(FILE *fp, char *readbuffer) {
   }
 }
 
-unsigned short get_byte_lcg() {
+unsigned short get_byte_lcg(void) {
   if (lcg_bits_left == 0) {
     lcg_iterate();
   }
@@ -237,7 +246,7 @@ unsigned short get_byte_lcg() {
   return res;
 }
 
-unsigned short get_word_lcg() {
+unsigned short get_word_lcg(void) {
   unsigned short res;
 
   if (lcg_bits_left == 0) {
@@ -261,7 +270,7 @@ unsigned short get_word_lcg() {
   return res;
 }
 
-unsigned long get_long_lcg() {
+unsigned long get_long_lcg(void) {
   if (lcg_bits != 0) {
     unsigned int res = lcg_bits;
     if (lcg_bits_left == 24) {
@@ -287,6 +296,76 @@ unsigned long get_long_lcg() {
   }
 }
 
+unsigned long get_val_lcg(void) {
+  unsigned long long tmp_val;
+  do {
+    if (opt_mode_size == 'b') {
+      tmp_val = get_byte_lcg();
+    } else if (opt_mode_size == 'w') {
+      tmp_val = get_word_lcg();
+    } else {
+      tmp_val = get_long_lcg();
+    }
+  
+    if (opt_scale != 0) {
+      /* 
+       * scale tmp_val into opt_min <= tmp_val <= opt_max. 
+       * opt_scale is opt_max - opt_min + 1
+       * opt_scale is <  2^opt_mode_size_bits
+       * original tmp_val has: 0 <= tmp_val < 2^opt_mode_size_bits
+       */
+      tmp_val *= opt_scale;
+      tmp_val = tmp_val >> opt_mode_size_bits;
+      tmp_val += opt_min;
+    }
+  
+  
+    if (opt_tri == 1) {
+      /*
+       * The following implements a rejection sampler to sample a 
+       * triangular distribution over the integer range from opt_min to 
+       * opt_max inclusive, with a peak probability at opt_minmax. 
+       */
+      unsigned long long test_val;
+      if (opt_mode_size == 'b') {
+        test_val = get_byte_lcg();
+      } else if (opt_mode_size == 'w') {
+        test_val = get_word_lcg();
+      } else {
+        test_val = get_long_lcg();
+      }
+      /*
+       * scale test_val into opt_min <= test_val < opt_minmax
+       * original test_val is: 0 <= test_val < 2^opt_mode_size_bits
+       * opt_minmax is (opt_min + opt_max)/2
+       * opt_minmax is < 2^opt_mode_size_bits
+       */
+      test_val *= (opt_minmax-opt_min);
+      test_val = test_val >> opt_mode_size_bits;
+      test_val += opt_min;
+      
+      unsigned long long check_val = tmp_val;
+      /*
+       * here is:
+       * opt_min <= check_val <= opt_max
+       * 0 <= opt_min <= opt_minmax < opt_max < 2^opt_mode_size_bits
+       * opt_minmax = (opt_min + opt_max)/2
+       * map check_val into opt_min <= check_val <= opt_minmax such that
+       * check_val = opt_max => opt_min
+       */
+      if (check_val > opt_minmax) {
+        check_val = opt_minmax*2 - check_val;
+      }
+      /* take tmp_val if check_val > test_val, else try new pair. */
+      if (check_val >test_val)
+        break;
+    } else {
+      /* no rejection sampling, take tmp_val unconditionaly. */
+      break;
+    }
+  } while(1);
+  return (unsigned long)tmp_val;
+}
 
 void fill_lcg(char *buf, unsigned int bytes) {
   unsigned int bytes_left = lcg_bits_left / 8;
@@ -407,9 +486,16 @@ int main(int argc, char **argv) {
 		case 'c':
 			opt_count = atoll(optarg);
 			break;
-		case 'b':
-		case 'w':
 		case 'l':
+      opt_mode_size_bits = +16;  /* mode_size_bits is pre-initialized with 8 */
+      opt_mode_size_mask |= 0xffff0000;
+      /* fall through */
+		case 'w':
+      opt_mode_size_bits += 8;
+      opt_mode_size_mask |= 0x0ff00;
+      /* fall through */
+		case 'b':
+      /* no change to opt_mode_size_bits, it is pre-initialized with 8 */
 			opt_mode_size = c;
       opt_mode_size_cnt += 1;
       break;
@@ -420,6 +506,14 @@ int main(int argc, char **argv) {
 			opt_mode_fmt = c;
       opt_mode_fmt_cnt += 1;
       break;
+    case 't':
+      opt_tri = 1;
+      break;
+    case 'v':
+      opt_verb += 1;
+      break;
+    case 'H':
+      /* fall through */
     case '?':
       opt_unknown = 1;
       break;
@@ -452,6 +546,30 @@ int main(int argc, char **argv) {
       return save_statefile();
     }
   }
+  
+  if (opt_max > 0) {
+    if (opt_max > opt_mode_size_mask) {
+      printf("max value '%ld' to large for data size '%c'\n", opt_max, opt_mode_size);
+      return 1;
+    }
+    if (opt_min >= opt_max) {
+      printf("max value '%ld' must be greater then min value '%ld'\n", opt_max, opt_min);
+      return 1;
+    }
+
+    opt_scale = opt_max - opt_min;
+    if ( (opt_min > 0) || (opt_max != 0xffffffff) )
+      opt_scale += 1;
+
+    unsigned long long tmp = opt_min;
+    tmp += opt_max;
+    tmp /= 2;
+    opt_minmax = (unsigned long)tmp;
+  } else {
+    opt_max = opt_mode_size_mask;
+    opt_minmax = opt_max/2;
+  }
+
 
   if (opt_unknown == 1) {
     print_help();
@@ -468,70 +586,79 @@ int main(int argc, char **argv) {
   if (err != 0)
     return 1;  /* error messages already printed by above functions. */
 
+  if (opt_verb > 1) {
+    fprintf(stderr, "opt_min %ld\n"
+                    "opt_max %ld\n"
+                    "opt_scale  %ld\n"
+                    "opt_minmax %ld\n"
+                    "opt_mode_size_bits %d\n"
+                    "opt_mode_size_mask %08lx\n"
+                    "opt_mode_size %c\n"
+                    "opt_mode_fmt  %c\n"
+                    "opt_triangle %s\n",
+                    opt_min, opt_max, opt_scale, opt_minmax,
+                    opt_mode_size_bits, opt_mode_size_mask,
+                    opt_mode_size, opt_mode_fmt,
+                    (opt_tri == 1) ? "yes" : "no"
+            );
+  }
+  
   unsigned long long baseconv = 0;
   int baseconv_bits = 0;
 
-  while (opt_count > 0) {
+  const char *cur_fmt = 0;
+  if (opt_mode_fmt == 'd') {
+    if (opt_mode_size == 'b' || opt_mode_size == 'w') {
+      cur_fmt = "%d\n";
+    } else {
+      cur_fmt = "%ld\n";
+    }
+  } else if (opt_mode_fmt == 'h') {
     if (opt_mode_size == 'b') {
-      unsigned int tmp_int = get_byte_lcg();
-      if (opt_mode_fmt == 'd') {
-        fprintf(stdout, "%d\n", tmp_int);
-      } else if (opt_mode_fmt == 'h') {
-        fprintf(stdout, "%02x\n", tmp_int);
-      } else if (opt_mode_fmt == 'a') {
-        baseconv |= tmp_int << baseconv_bits;
-        baseconv_bits += 8;
-        while (baseconv_bits >= 6) {
-          fprintf(stdout, "%c", 32 + (unsigned int)(baseconv & 0x03f));
-          baseconv = baseconv >> 6;
-          baseconv_bits -= 6;
-        }
-      } else {
-        fprintf(stdout, "%c", tmp_int); /* no newline */
-      }
+      cur_fmt = "%02x\n";
     } else if (opt_mode_size == 'w') {
-      unsigned short tmp_short = get_word_lcg();
-      unsigned char *tmp_short_p = (unsigned char *)&tmp_short;
-      if (opt_mode_fmt == 'd') {
-        fprintf(stdout, "%d\n", (unsigned int)tmp_short);
-      } else if (opt_mode_fmt == 'h') {
-        fprintf(stdout, "%04x\n", (unsigned int)tmp_short);
-      } else if (opt_mode_fmt == 'a') {
-        baseconv |= ((unsigned long long)tmp_short) << baseconv_bits;
-        baseconv_bits += 16;
-        while (baseconv_bits >= 6) {
-          fprintf(stdout, "%c", 32 + (unsigned int)(baseconv & 0x03f));
-          baseconv = baseconv >> 6;
-          baseconv_bits -= 6;
-        }
-/*        fprintf(stdout, "%c%c%c\n", 32 + (tmp_short & 0x03f), 32 + ((tmp_short & 0x0fc0) >> 6), 32 + ((tmp_short & 0x03f000) >> 12)); */
-      } else {
+      cur_fmt = "%04x\n";
+    } else {
+      cur_fmt = "%08lx\n";
+    }
+  }
+
+  while (opt_count > 0) {
+    unsigned long tmp_val = get_val_lcg();
+    if (opt_mode_fmt == 'o') {
+      /* mode o, size b, w or l */
+      if (opt_mode_size == 'b') {
+        unsigned int tmp_int = (unsigned int)tmp_val;
+        fprintf(stdout, "%c", tmp_int); /* no newline */
+      } else if (opt_mode_size == 'w') {
+        unsigned short tmp_short = (unsigned short)tmp_val;
+        unsigned char *tmp_short_p = (unsigned char *)&tmp_short;
         fprintf(stdout, "%c%c", tmp_short_p[0], tmp_short_p[1]); /* no newline */
+      } else {
+        unsigned long tmp_long = (unsigned long)tmp_val;
+        unsigned char *tmp_long_p = (unsigned char *)&tmp_long;
+        fprintf(stdout, "%c%c%c%c", tmp_long_p[0], tmp_long_p[1], tmp_long_p[2], tmp_long_p[3]); /* no newline */
       }
     } else {
-      unsigned long tmp_long = get_long_lcg();
-      unsigned char *tmp_long_p = (unsigned char *)&tmp_long;
-      if (opt_mode_fmt == 'd') {
-        fprintf(stdout, "%ld\n", tmp_long);
-      } else if (opt_mode_fmt == 'h') {
-        fprintf(stdout, "%08lx\n", tmp_long);
-      } else if (opt_mode_fmt == 'a') {
-        baseconv |= ((unsigned long long)tmp_long) << baseconv_bits;
-        baseconv_bits += 32;
+      /* mode a,d or h, size b, w or l */
+      if (opt_mode_fmt == 'a') {
+      /* mode a, size b, w or l */
+        baseconv |= ((unsigned long long)tmp_val) << baseconv_bits;
+        baseconv_bits += opt_mode_size_bits;
         while (baseconv_bits >= 6) {
-          fprintf(stdout, "%c", 32 + (unsigned int)(baseconv & 0x03f));
+          fprintf(stdout, "%c", 48 + (unsigned int)(baseconv & 0x03f));
           baseconv = baseconv >> 6;
           baseconv_bits -= 6;
         }
-/*        int i;
-        for (i = 0; i < 7; i++) {
-          fprintf(stdout, "%c", 32 + (unsigned int)(tmp_long & 0x03f));
-          tmp_long = tmp_long >> 6;
-        }
-        fprintf(stdout, "\n");
-*/
       } else {
-        fprintf(stdout, "%c%c%c%c", tmp_long_p[0], tmp_long_p[1], tmp_long_p[2], tmp_long_p[3]); /* no newline */
+        /* mode d or h, size b, w or l */
+        if (opt_mode_size == 'l') {
+          /* mode d or h, size l */
+          fprintf(stdout, cur_fmt, (unsigned long)tmp_val);
+        } else {
+          /* mode d or h, size b or w */
+          fprintf(stdout, cur_fmt, (unsigned int)tmp_val);
+        }
       }
     }
     opt_count -= 1;
