ced65fbd3080c50887e92e66a61b5b01e3b1a6be
BjoKaSH <googlelogin@bjoern-kahl.de>
2012-11-18 02:33:00 +0100

    Add new function for pool creation.  Added support functions for file generation. New functions for testing size accounting (incomplete).

--- support/run-tests.sh
+++ support/run-tests.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
 set -x -v
+export LC_ALL=C 
 
 # load configuration, if present
 #
 # set defaults
 poolbase=pool_$(date +%s)_$$
 has_fstest=0
+genrand_bin=./support/genrand
 #
 # check for local config file
 if [ -f maczfs-tests.conf] ; then
@@ -32,6 +34,15 @@ fi
 if [ -z "${tests_logdir}" ] ; then
     tests_logdir=$(mktemp -d -t test_logs_maczfs_)
 fi
+
+if [ ! -x "${genrand_bin}" ] ; then
+    echo "Error: random number generator '' not found."
+    echo "Did you compiled it? ('gcc -o genrand -O3 genrand.c' in the support folder.)"
+fi
+
+# initialize random datas generator
+genrand_state=${tests_logdir}/randstate.txt
+${genrand_bin} -s 13446 -S ${genrand_state}
 
 
 # load various helper functions
@@ -61,18 +72,20 @@ run_ret 0 "Create disk vd1" make_disk 5 vd1 8
 attach_disk vd1
 #run_ret 0 "Partition disk vd1" partion_disk vd1
 
-pool1=${poolbase}_p1
-run_ret 0 "Create zpool ${pool1} with vdev vd1 at ${vd1_disk}s2" zpool create ${pool1} ${vd1_disk}s2
+run_ret 0 "Create zpool ${pool1} with vdev vd1 at ${vd1_disk}s2" make_pool p1 ${vd1_disk}s2
+pool1=${pool_p1_fullname}
+pool1path=${pool_p1_path}
 
 run_check_regex 0 "Checking it auto-mounted" "${pool1}" mount
 
-run_ret 0 "Unmount using diskutil" diskutil umount /Volumes/${pool1}
+run_ret 0 "Unmount using diskutil" diskutil umount ${pool1path}
 run_check_regex 0 "Verifying unmount" '-n' "${pool1}" mount
 
 run_ret 0 "Remounting using zfs utility" zfs mount ${pool1}
 run_check_regex 0 "Verifying mount" "${pool1}" mount
 
 sleep 2
+echo "Turning of indexing on test pool(s)"
 mdutil -i off /Volumes/${pool1}
 
 sleep 5
@@ -91,21 +104,48 @@ run_check_regex 0 "Verifying import" "${pool1}" mount
 if [ ${has_fstest} -eq 1 ] ; then
     echo "Running fstest suite ..."
     ((curtest++))
+    mkdir ${pool1path}/fstest
+    pushd ${pool1path}/fstest
     run_ret 0 "" run-fstest.sh
     res=$?
+    popd
     echo -n "Completed fstest suite"
     print_count_ok_fail ${res}
 fi
 
 
-
 # - create large (>8 MB), compressible (factor > 2) file as "tf1"
 #   - verify space accounting
+
+run_cmd_log -t 0 get_fs_stats stat_p1_a p1
+run_ret 0 -t 1 "Creating 8m file (random, compressible)" make_file -c 2 8m _temp_ tf0
+run_ret 0 -t 2 "" copy_file tf0 p1 tf1
+
+run_cmd_log -t 0 get_file_stats stat_tf1_b tf1
+run_cmd_log -t 1 get_fs_stats stat_p1_b p1
+diff_fs_stats stat_p1_diff1  stat_p1_a  stat_p1_b
+run_ret_start 0 -t 2 "verify space accounting" check_sizes_fs  stat_p1_diff1  8m  
+run_ret_end 0 check_sizes_file  stat_tf1_b  8m
+
 # - enable compression
+run_ret 0 "Enabling compression" zfs set compression=on ${pool1}
+
 # - write same file under new name "tf2" again
 #   - verify it got compressed
 #   - verify du, df, ls and stat return sensible values
 #   - verify content of uncompressed file
+
+run_cmd_log -t 0 get_fs_stats stat_p1_tf2_a  p1
+run_ret_start 0 -t 1 "Writing same 8m file, then checking compression" copy_file tf1 tf2
+calc_comp_fact tf0
+run_cmd_log -t 2 get_file_stats stat_tf2 tf2
+run_ret_next 0 -t 3 "verifying file size" check_sizes_file -c ${file_tf2_compfact}  stat_tf1_b  8m
+run_cmd_log -t 4 get_fs_stats stat_p1_tf2_b  p1
+diff_fs_stats  stat_p1_tf2_diff1  stat_p1_tf2_a  stat_p1_tf2_b
+run_ret_next 0 -t 5 "verifying fs size" check_sizes_fs -c ${file_tf2_compfact}  stat_p1_tf2_diff 8m 
+run_ret_next 0 "comparing content of uncompressed file"  cmp ${file_tf0_path} ${file_tf1_path}
+run_ret_end 0 "comparing content of compressed file" cmp ${file_tf0_path} ${file_tf2_path}
+
 # - create snapshot "sn1"
 # - set copies = 2
 # - write same file again as "tf3"
--- support/tests-functions.sh
+++ support/tests-functions.sh
@@ -8,6 +8,16 @@
 # new_temp_file [ -p ]
 # stdout: filename, with path if "-p" given.
 #
+# make_pool poolname [ -o option=value [ -o ... ] ] vdevs ...
+# retval: return code of zpool create
+# globals: pool_${poolname}_opt, pool_${poolname}_path, pool_${poolname}_vdevs, pool_${poolname}_fullname
+#
+# make_fs fsname [ -o option=value [ -o ... ] ]
+# globals: pool_${fsname_tr}_opt=, pool_${fsname_tr}_path, pool_${fsname_tr}_name, pool_${fsname_tr}_fullname
+#
+# make_file [ -c comp_factor ] size pool file
+# globals: file_${file}_size, file_${file}_pool, file_${file}_path, file_${file}_name
+#
 # run_cmd [ --outname tmpfile | --outarray varname ] [ --errname tmpfile | --errarray varname ] command [ args ... ]
 # retval: return code of executed command
 #
@@ -90,6 +100,448 @@ function new_temp_file() {
     return ${res}
 }
 
+
+# create named zfs pool
+# args:
+# poolname [ -o option=value [ -o ... ] ] vdevs ...
+# globals:
+# pool_${poolname}_opt=...   : options used
+# pool_${poolname}_fullname= : pool name with pool prefix
+# pool_${poolname}_path=     : full path to pool filesystem
+# pool_${poolname}_vdevs=... : vdev specification used
+# poolbase : prefix for all pools.
+function make_pool() {
+    local opt=""
+    local poolpath=""
+    local poolname="${1}"
+    local poolfullname="${poolbase}_${poolname}"
+
+    shift
+    while [ "$1" == "-o" ] ; do
+        shift
+        opt="${opt} $1"
+        shift
+    done
+    eval pool_${poolname}_opt="\"\${opt}\""
+    eval pool_${poolname}_fullname="\"\${poolfullname}\""
+    eval pool_${poolname}_path="\"/Volumes/\${poolfullname}\""
+    eval pool_${poolname}_vdevs="\"\$*\""
+
+    zpool create ${opt} ${poolname} $*
+}
+
+
+# create named zfs filesystem
+# args:
+# fsname [ -o option=value [ -o ... ] ]
+# globals:
+# (fsname_tr is fsname, but with "/" mapped to "_")
+# pool_${fsname_tr}_opt=...  : options used
+# pool_${fsname_tr}_path=    : full path to zfs filesystem
+# pool_${fsname_tr}_name=... : original file system name ${fsname}
+# pool_${fsname_tr}_fullname= : zfs filesystem name with full pool prefix
+function make_fs() {
+    local opt=""
+    local fspath=""
+    local fsname="${1}"
+    local fsname_tr="${fsname//\//_}"
+    local fsfullname=""
+
+    # if fsname and fsname_tr are identical, then we have only the pool fs itself, which is forbidden.
+    if [ "${fsname}" == "${fsname_tr}" ] ; then
+        echo "Error: make_fs() requires at least one "/" in the fs name"
+        return 1
+    fi
+    fsfullname=${poolbase}_${fsname}
+
+    shift
+    while [ "$1" == "-o" ] ; do
+        shift
+        opt="${opt} $1"
+        shift
+    done
+    eval pool_${fsname_tr}_opt="\"\${opt}\""
+    eval pool_${fsname_tr}_name="\"\${fsname}\""
+    eval pool_${fsname_tr}_fullname="\"\${fsfullname}\""
+    eval pool_${fsname_tr}_path="\"/Volumes/\${poolfullname}\""
+
+    zfs create ${opt} ${fsname}
+}
+
+
+# create a (temporary) file of given size
+# args:
+# [ -c comp_factor ] size pool file
+# -c comp_factor : try to make the file contents compressible by factor comp_factor
+# size  : size of file in bytes, optionaly with multiplier "m" or "k", example: 8m
+# pool  : zfs filesystem to place file in.  if special name _temp_ is used, then place file in ${TMPDIR}
+# file  : filename (optionally with path prefix) relative to zfs filesystem
+# globals:
+# file_${file}_size = size of file
+# file_${file}_pool = zfs filessystem
+# file_${file}_name = original file name
+# file_${file}_path = full path to file, starting at "/"
+# pool_${pool}_path : path to zfs filesystem
+function make_file() {
+    local filename=""
+    local filename_tr=""
+    local size=""
+    local pool=""
+    local filepath=""
+    local compfact=0
+    local count=0
+    local sizeflag=""
+    local poolpath_v=""
+
+    if [ "${1}" == "-c" ] ; then
+        shift
+        compfact=${1}
+        shift
+    fi
+
+    size=${1}
+    pool=${2}
+    filename=${3}
+    filename_tr=${filename//\//_}
+    
+    if [ "${size: -1:1}" == "m" ] ; then
+        # 1m bytes makes 1024*256 longs
+        count=$((${size%m}*1024*256))
+        size=$((${count*4}))
+        sizeflag=-m
+    elif [ "${size: -1:1}" == "k" ] ; then
+        # 1k bytes makes 256 longs
+        count=$((${size%k}*256))
+        size=$((${count*4}))
+        sizeflag=-m
+    else
+        # size is given in bytes
+        count=${size}
+        sizeflag=""
+    fi
+
+    if [ "${pool}" == "_temp_" ] ; then
+        filepath=$(new_temp_file)
+    else
+        poolpath_v=pool_${pool//\//_}_path
+        filepath=${!poolpath_v}/${filename}
+    fi
+    filedir=$(dirname ${filepath})
+    if [ ! -e ${filedir} ] ; then
+        mkdir -pv ${filedir}
+    elif [ ! -d ${filedir} ] ; then
+        echo "Error: file path is not a directory."
+        return 1
+    fi
+
+    if [ ${compfact} -eq 0 ] ; then
+        ${genrand_bin} -S ${genrand_state} -c ${count} ${sizeflag} -o >${filepath}
+        res=$?
+    else
+        ${genrand_bin} -S ${genrand_state} -c ${count} ${sizeflag} -o -t >${filepath}
+        res=$?
+    fi
+    
+    if [ $res -eq 0 ] ; then
+        eval file_${filename_tr}_size=${size}
+        eval file_${filename_tr}_pool=${pool}
+        eval file_${filename_tr}_name=${filename}
+        eval file_${filename_tr}_path=${filepath}
+    else
+        echo "Error: generating file ${filepath} (${filename}) failed"
+    fi
+    
+    return $res
+}
+
+
+# determine file statistics and store result in the array destvar as
+# well as in individual variables ${destvar}_*
+# args:
+# destvar file
+# destvar : name of an array, which will be set to the file statistics
+#    the array will be index starting with 0 in the same order as the
+#    variables listed under 'globals'.
+# file : file name to use.  the actual path is read from
+#    file_${file}_path, see make_file().
+# globals:
+# ${destvar}_name
+# ${destvar}_path
+# ${destvar}_mode
+# ${destvar}_nlink
+# ${destvar}_uid
+# ${destvar}_gid
+# ${destvar}_size
+# ${destvar}_atime
+# ${destvar}_mtime
+# ${destvar}_ctime
+# ${destvar}_blksize
+# ${destvar}_blocks
+function get_file_stats() {
+    local destvar=$1
+    local filename=$2
+    local filepath_v=file_${filename//\//_}_path
+    local filepath=${!filepath_v}
+
+    if [ ! -e "${filepath}" ] ; then
+        echo "Error: file '${filepath}' does not exists."
+        return 1
+    fi
+
+    local st_dev
+    local st_ino
+    local st_mode
+    local st_nlink
+    local st_uid
+    local st_gid
+    local st_rdev
+    local st_size
+    local st_atime
+    local st_mtime
+    local st_ctime
+    local st_birthtime
+    local st_blksize
+    local st_blocks
+    local st_flags
+    
+    local i
+    for i in $(stat -s ${filepath}) ; do
+        eval $i
+    done
+
+    eval ${destvar}_name=${filename}
+    eval ${destvar}_path=${filepath}
+
+    local i2=0
+    for i in mode nlink uid gid size atime mtime ctime blksize blocks ; do
+        eval ${destvar}_${i}=\$\{st_${i}\}
+        eval ${destvar}\[${i2}\]=\$\{st_${i}\}
+        ((i2++))
+    done
+}
+
+
+# determine zfs fs statistics
+# args:
+# destvar fs
+# destvar : name of an array, which will be set to the file statistics
+#    the array will be index starting with 0 in the same order as the
+#    variables listed under 'globals'.
+# fs : file name to use.  the actual path is read from
+#    file_${file}_path, see make_file().
+# globals:
+# ${destvar}_pool   = derived from pool_${fs}_name
+# ${destvar}_name   = copied from pool_${fs}_name
+# ${destvar}_fullname = copied from pool_${fs}_fullname
+# ${destvar}_path   = copied from pool_${fs}_path
+# ${destvar}_size   = pool size, from zpool list
+# ${destvar}_alloc  = allocated space in pool, from zpool list
+# ${destvar}_free   = free space in pool, from zpool list
+# ${destvar}_used   = allocate space in fs, from zfs list
+# ${destvar}_avail  = free space in fs, from zfs list
+# ${destvar}_ref    = refered  space in fs, from zfs list
+# ${destvar}_comp   = compression ratio, zero if disabled
+# ${destvar}_dfblocks = total blocks, from df -k
+# ${destvar}_dffree = free space, from df -k
+# ${destvar}_dfused = allocated space, from df -k
+function get_fs_stats() {
+    local destvar=$1
+    local fs=$2
+    local fs_tr=${fs//\//_}
+    local pool=${fs%%/*}
+    local tmp_v=pool_${fs_tr}_fullname
+    local fullname=${!tmp_v}
+    local poolfullname=${fullname%%/*}
+    tmp_v=pool_${fs_tr}_path
+    local path=${!tmp_v}
+    local idx=0
+    local tmp_res
+    local idx2=0
+    local i
+
+    eval ${destvar}_pool=${pool}
+    eval ${destvar}_name=${fs}
+    eval ${destvar}_fullname=${fullname}
+    eval ${destvar}_path=${path}
+
+    for i in pool name fullname path ; do
+        tmp_v=${destvar}_${i}
+        eval ${destvar}\[${idx}\]=${!tmp_v}
+        ((idx++))
+    done
+
+    tmp_res=($(LC_ALL=C zpool list ${poolfullname} | LC_ALL=C gawk "/${poolfullname}/"' {print $2 "\n" $3 "\n" $4 "\n";}') )
+
+    for ((idx2=0; idx2 < 3; idx2++)) ;
+        tmp_val=${tmp_res[$idx]}
+        unit=${tmp_val: -1:1}
+        if [ "${unit}" == "T" ] ; then
+            tmp_val=$( echo "${tmp_val%T}*1024*1024" | bc)
+        elif [ "${unit}" == "G" ] ; then
+            tmp_val=$( echo "${tmp_val%G}*1024" | bc)
+        elif [ "${unit}" == "K" ] ; then
+            tmp_val=$( echo "${tmp_val%K}/1024" | bc)
+        else
+            tmp_val=${tmp_val%M}
+        fi
+        tmp_res[$idx]=${tmp_val}
+    done
+
+    idx2=0
+    for i in size alloc free ; do
+        eval ${destvar}_${i}=${tmp_res[${idx2}]}
+        eval ${destvar}\[${idx}\]=${tmp_res[${idx2}]}
+        ((idx++))
+        ((idx2++))
+    done
+
+    tmp_res=($(zfs list ${fullname} | gawk "/${fullname}/"' {print $2 "\n" $3 "\n" $4 "\n";}') )
+    if [ "$(zfs get compression ${fullname} | gawk "/${fullname}/"' {print $3;}')" == "on" ] ; then
+        tmp_res[3]=$(zfs get compressratio ${fullname} | gawk "/${fullname}/"' {print $3;}')
+    else
+        tmp_res[3]=0
+    fi
+
+    for ((idx2=0; idx2 < 3; idx2++)) ;
+        tmp_val=${tmp_res[$idx]}
+        unit=${tmp_val: -1:1}
+        if [ "${unit}" == "T" ] ; then
+            tmp_val=$( echo "${tmp_val%T}*1024*1024" | bc)
+        elif [ "${unit}" == "G" ] ; then
+            tmp_val=$( echo "${tmp_val%G}*1024" | bc)
+        elif [ "${unit}" == "K" ] ; then
+            tmp_val=$( echo "${tmp_val%K}/1024" | bc)
+        else
+            tmp_val=${tmp_val%M}
+        fi
+        tmp_res[$idx]=${tmp_val}
+    done
+
+    idx2=0
+    for i in used avail ref comp ; do
+        eval ${destvar}_${i}=${tmp_res[${idx2}]}
+        eval ${destvar}\[${idx}\]=${tmp_res[${idx2}]}
+        ((idx++))
+        ((idx2++))
+    done
+
+    tmp_res=($(df -k -P ${path} | gawk "/${fullname//\//\\/}/"' {print $2 "\n" $3 "\n" $4 "\n"}') )
+
+    idx2=0
+    for i in dfblocks dfused dffree; do
+        eval ${destvar}_${i}=${tmp_res[${idx2}]}
+        eval ${destvar}\[${idx}\]=${tmp_res[${idx2}]}
+        ((idx++))
+        ((idx2++))
+    done
+}
+
+
+# compare two fs stats and save difference in new array
+# args:
+# diff_array  stats_1_array  stats_2_array
+# globals:
+# ${diff_array}_pool   = copied from stats_1_array
+# ${diff_array}_name   = copied from stats_1_array
+# ${diff_array}_fullname = copied from stats_1_array
+# ${diff_array}_path   = copied from stats_1_array
+# ${diff_array}_size   = stats_2_array - stats_1_array
+# ${diff_array}_alloc  = stats_2_array - stats_1_array
+# ${diff_array}_free   = stats_2_array - stats_1_array
+# ${diff_array}_used   = stats_2_array - stats_1_array
+# ${diff_array}_avail  = stats_2_array - stats_1_array
+# ${diff_array}_ref    = stats_2_array - stats_1_array
+# ${diff_array}_comp   = stats_2_array - stats_1_array
+# ${diff_array}_dfblocks = stats_2_array - stats_1_array
+# ${diff_array}_dfused = stats_2_array - stats_1_array
+# ${diff_array}_dffree = stats_2_array - stats_1_array
+#
+function diff_fs_stats() {
+    local destvar=$1
+    local prevar=$2
+    local postvar=$3
+    local idx=0
+    local i=0
+    local i2=0
+    local temp_v=""
+    local preval
+    local postval
+    local diffval
+    
+    for i in pool name fullname path ; do
+        temp_v=${prevar}_${i}
+        preval=${!temp_v}
+        temp_v=${postvar}_${i}
+        postval=${!temp_v}
+        eval ${destvar}_${i}=${preval}
+        eval ${destvar}\[${idx}\]=${preval}
+        ((idx++))
+    done
+
+    for i in size alloc free used avail ref comp ; do
+        temp_v=${prevar}_${i}
+        preval=${!temp_v}
+        temp_v=${postvar}_${i}
+        postval=${!temp_v}
+        diffval=$(echo "${postval} - ${preval}" | bc)
+        eval ${destvar}_${i}=${diffval}
+        eval ${destvar}\[${idx}\]=${diffval}
+        ((idx++))
+    done
+
+    for i in dfblocks dfused dffree ; do
+        temp_v=${prevar}_${i}
+        preval=${!temp_v}
+        temp_v=${postvar}_${i}
+        postval=${!temp_v}
+        diffval=$((${postval} - ${preval}))
+        eval ${destvar}_${i}=${diffval}
+        eval ${destvar}\[${idx}\]=${diffval}
+        ((idx++))
+    done
+}
+
+
+# check if fs size change matches file creation / deletion.
+# args:
+# [ -c compfact ] diff_stat_array  size
+# -c compfact : expected compression factor of file
+# diff_stat_array : result from diff_fs_stats()
+# size  : uncompressed file size added to or removed from file system 
+# globals:
+# ${diff_array}_pool   = copied from stats_1_array
+# ${diff_array}_name   = copied from stats_1_array
+# ${diff_array}_fullname = copied from stats_1_array
+# ${diff_array}_path   = copied from stats_1_array
+# ${diff_array}_size   = stats_2_array - stats_1_array
+# ${diff_array}_alloc  = stats_2_array - stats_1_array
+# ${diff_array}_free   = stats_2_array - stats_1_array
+# ${diff_array}_used   = stats_2_array - stats_1_array
+# ${diff_array}_avail  = stats_2_array - stats_1_array
+# ${diff_array}_ref    = stats_2_array - stats_1_array
+# ${diff_array}_comp   = stats_2_array - stats_1_array
+# ${diff_array}_dfblocks = stats_2_array - stats_1_array
+# ${diff_array}_dfused = stats_2_array - stats_1_array
+# ${diff_array}_dffree = stats_2_array - stats_1_array
+function check_sizes_fs() {
+    local compfact=0
+    local size=0
+
+    if [ "${1}" == "-c" ] ; then
+        shift
+        compfact=${1}
+        shift
+        size=$(parse_size $1)
+        sizecomp=$( echo ${size} / ${compfact} | bc)
+    else
+        compfact=1
+        size=$(parse_size $1)
+        sizecomp=${size}
+    fi
+
+    blockcount=$( echo "(${sizecomp} / ${blocksize}) + 1" | bc)
+
+}
 
 # run a command and optionally capture stdout and/or stderr into files or arrays
 # args:
@@ -359,30 +811,59 @@ function partion_disk() {
 # ${tests_logdir}/test_${curtest}.X where X is "cmd", "out" or
 # "err", respectively.
 # args:
-# command [ args .. ]
+# [ -t subtest ] command [ args .. ]
+# -t subtest  : sub test number, if present and > 0 suppresses increment of curtest
 # globals:
-# curtest : incremented by 1, if message given
+# curtest : incremented by 1, if subnum not given or 0
 function run_cmd_log() {
-    ((curtest++))
+    local subnum=0
+    local logname=""
 
-    echo "$*"  >${tests_logdir}/test_${curtest}.cmd
+    if [ "$1" == "-t" ] ; then
+        shift
+        subnum=$1
+        shift
+        if [ ${subnum} -eq 0 ] ; then
+            ((curtest++))
+        fi
+        logname=${curtest}.${subnum}
+    else
+        ((curtest++))
+        logname=${curtest}
+    fi
 
-    run_cmd  --outname ${tests_logdir}/test_${curtest}.out  --errname ${tests_logdir}/test_${curtest}.err  "$@"
+    echo "$*"  >${tests_logdir}/test_${logname}.cmd
+
+    run_cmd  --outname ${tests_logdir}/test_${logname}.out  --errname ${tests_logdir}/test_${logname}.err  "$@"
 }
 
 
 # print a log previously captured by run_cmd_log.
 #
 # args:
+# [ -t subtest ]
+# -t subtest  : sub test number to use
 function print_run_cmd_logs() {
-    if [ -s ${tests_logdir}/test_${curtest}.out ] ; then
-        echo "out:"
-        gawk '{print " | " $0;}' <${tests_logdir}/test_${curtest}.out
+    local subtest=0
+    local logname=""
+
+    if [ $# -gt 1 -a "$1" == "-t" ] ; then
+        shift
+        subtest=$1
+        shift
+        logname=${curtest}.${subtest}
+    else
+        logname=${curtest}
     fi
 
-    if [ -s ${tests_logdir}/test_${curtest}.err ] ; then
+    if [ -s ${tests_logdir}/test_${logname}.out ] ; then
+        echo "out:"
+        gawk '{print " | " $0;}' <${tests_logdir}/test_${logname}.out
+    fi
+
+    if [ -s ${tests_logdir}/test_${logname}.err ] ; then
         echo "err:"
-        gawk '{print " | " $0;}' <${tests_logdir}/test_${curtest}.err
+        gawk '{print " | " $0;}' <${tests_logdir}/test_${logname}.err
     fi
 }
 
@@ -414,28 +895,45 @@ function print_count_ok_fail() {
 # "cmd", "out" or "err", respectively, if the message parameter is not
 # the empty string.  Returns zero if command's return value matches
 # expected_retval, else returns 1.
+# if -t subtest is given, then the lognames are changed to
+# ${tests_logdir}/test_${curtest}.${subtest}.X with X as given above.
 # args:
-# expected_retval message command [ args ]
+# expected_retval [ -t subtest ] message command [ args ]
 # globals:
 # last_cmd_retval : set to return value from command
-# curtest : incremented by 1, if message given
+# curtest : incremented by 1, if message given and subtest > 0 or not present
 # okcnt   : incremented by 1, if message given and return value matches
 # failcnt : incremented by 1, if message given and return value dose not match
 function run_ret() {
     local exp_ret=$1
     local message="$2"
+    local subtest=0
+    local subtestarg=""
     local retval=0
     shift
     shift
 
+    if [ "${message}" == "-t" ] ; then
+        subtest=$1
+        subtestarg="-t ${subtest}"
+        shift
+        message="$1"
+        shift
+    fi
+
     if [ -z "${message}" ] ; then
-        run_cmd "$@"
-        retval=$?
+        if [ -z "${subtestarg}" ] ; then
+            run_cmd "$@"
+            retval=$?
+        else
+            run_cmd_log ${subtestarg} "$@"
+            retval=$?
+        fi
     else
         echo -n "${message}"
         echo -n -e "\t "
 
-        run_cmd_log "$@"
+        run_cmd_log ${subtestarg} "$@"
         retval=$?
 
         if [ ${retval} -eq ${exp_ret} ] ; then
